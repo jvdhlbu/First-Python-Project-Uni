13318111831c9aa315059496087b8db5e65a35cb
jvdhlbu <116588577+jvdhlbu@users.noreply.github.com>
2022-12-13 19:51:47 +0000

    Update task1.py

--- task1.py
+++ task1.py
@@ -8,11 +8,18 @@ if __name__ == '__main__':
     ADJECTIVES = ["peculiar", "garrulous", "ineluctable", "innocuous", "tractable", "vexatious", "ineffable", "intrinsic", "cogent", "contemporaneous"]
     ADVERBS = ["assiduously", "solicitously", "luxuriantly", "tremulously", "maniacally", "hydroponically", "precipitously", "sonorously", "industriously", "beseechingly"]
 
-    password_amount = int(input("How many passwords are needed? "))
+    print("Password Generator\n==================\n")
 
-    while password_amount <1 or password_amount >24:
-        print("Please enter a value between 1 and 24")
-        password_amount = int(input("How many passwords are needed? "))
+    while True:
+        try:
+            password_amount = int(input("How many passwords are needed? "))
+            if 0 < password_amount <= 24:
+                break
+            else:
+                print("Enter a value between 1 and 24")
+
+        except ValueError:
+            print("Please enter a numerical value")
 
     for i in range(password_amount):
         print(i + 1, "-->", random.choice(ADVERBS) + random.choice(ADJECTIVES) + random.choice(NOUNS))
