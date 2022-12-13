7b12cb50dcc76c51621e1d2d0b400b84c8c557ba
jvdhlbu <116588577+jvdhlbu@users.noreply.github.com>
2022-12-13 02:53:23 +0000

    Create task1.py

---
+++ task1.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import random
+
+if __name__ == '__main__':
+
+    NOUNS = ["aglet", "spile", "snood", "cacophony", "paradigm", "precipice", "reagent", "secateurs", "hackle", "directorate"]
+    ADJECTIVES = ["peculiar", "garrulous", "ineluctable", "innocuous", "tractable", "vexatious", "ineffable", "intrinsic", "cogent", "contemporaneous"]
+    ADVERBS = ["assiduously", "solicitously", "luxuriantly", "tremulously", "maniacally", "hydroponically", "precipitously", "sonorously", "industriously", "beseechingly"]
+
+    password_amount = int(input("How many passwords are needed? "))
+
+    while password_amount <1 or password_amount >24:
+        print("Please enter a value between 1 and 24")
+        password_amount = int(input("How many passwords are needed? "))
+
+    for i in range(password_amount):
+        print(i + 1, "-->", random.choice(ADVERBS) + random.choice(ADJECTIVES) + random.choice(NOUNS))
