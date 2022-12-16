aa352fcd951c84853bde1aa359ec53e8137191e5
jvdhlbu <116588577+jvdhlbu@users.noreply.github.com>
2022-12-16 22:46:40 +0000

    Update task3.py

--- task3.py
+++ task3.py
@@ -38,3 +38,60 @@ if __name__ == '__main__':
 
         #email_file.write(everything you want it to show)
 
+        
+        
+        --------------------------
+        
+        
+        
+        #!/usr/bin/env python3
+
+import random
+import sys
+import re
+import os
+
+if __name__ == '__main__':
+
+    student_file = open("students.txt", 'r')
+    email_file = open("emails.txt", 'w')
+
+    if not os.path.exists("students.txt"):
+        print("Error: file cannot be located.")
+        sys.exit(2)
+
+
+    # for each student in student_file, append onto ucas_numbers the first 8 characters
+    # of each iteration of student in the for loop.
+    try:
+        for student in student_file:
+            ucas_numbers = student[:8])
+            student_names = student[9:].split(", ")
+
+        for name in student_names:
+            first_name = student_names[1]
+            second_name = student_names[0]
+            #second_name = re.sub(r"[^a-zA-Z]", "", student_names[0])
+            #initials.append(first_name[0])
+            random_num = random.randint(1000, 9999)
+
+            initials = ".".join(chars for chars in first_name if chars.isupper())
+
+            #initials = for chars in first_name:
+                #if chars.isupper():
+                    #joined_chars = ".".join(chars)
+
+            emails = f"{initials}.{second_name}{random_num}@poppleton.ac.uk"
+
+        email_file.write(ucas_numbers + emails.lower() + "\n")
+
+    #emails = (joined_chars, ".", second_name, random_num, "@poppleton.ac.uk")
+
+
+        student_file.close()
+        email_file.close()
+
+    except FileNotFoundError:
+        print("Error. File cannot be located")
+
+    sys.exit(1)
